453fed4395cbb4d7daac02ac9f89234c1aee1497
PiyushDhirwani <piyush.dhirwani@iitgn.ac.in>
2023-06-22 08:33:59 +0530

    image pdf to text

--- temp.py
+++ temp.py
@@ -1,6 +1,5 @@
 from pdf2image import convert_from_path
 from pytesseract import image_to_string
-from fpdf import FPDF
 
 def convert_pdf_to_img(pdf_file):
     """
@@ -39,35 +38,16 @@ def get_text_from_any_pdf(pdf_file):
     @returns:
         - the textual content of ALL the pages
     """
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=15)
     images = convert_pdf_to_img(pdf_file)
     final_text = ""
     for pg, img in enumerate(images):
         
-        final_text = convert_image_to_text(img)
-        final_text = final_text.replace('\u2018', "'").replace('\u2019', "'").replace('\u2014', '-')
-        for i in final_text.split('\n'):
-            pdf.cell(200, 10, txt=i, ln=1,align='L')
-        
-        # pdf.cell(200, 10, txt=final_text, ln=1, align='C')
-
+        final_text += convert_image_to_text(img)
         #print("Page n°{}".format(pg))
         #print(convert_image_to_text(img))
-    pdf.output("GFG.pdf")
+    
     return final_text
 
 path_to_pdf = 'Testing.pdf'
 
 print(get_text_from_any_pdf(path_to_pdf))
-
-
-
-# Python program to create
-# a pdf file
-
-
-
-
-# Replace the right 
